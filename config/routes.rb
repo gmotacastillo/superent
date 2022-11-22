d79a3d31b4229f6ad89589d3c7575dd546ef974e
Gerard Mota <gmotacastillo@gmail.com>
2022-11-22 15:06:54 +0100

    created bookings model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :cars
+  resources :cars do
+    resources :bookings, only: [:index, :new, :create]
+  end
+  resources :bookings, only: [:show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
