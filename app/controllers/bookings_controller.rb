9ee9b4f0de014a8df1398491f1597ab85feac34b
Gerard Mota <gmotacastillo@gmail.com>
2022-11-22 16:22:34 +0100

    added bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+  before_action :set_car, only: %i[new create]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.car = @car
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
