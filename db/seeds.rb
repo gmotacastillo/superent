13e03a1233225d2f6adab40c6aadad1b233d940e
Gerard Mota <gmotacastillo@gmail.com>
2022-11-22 17:14:55 +0100

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+
+user = User.create!(email: "info@supereqwnt.com", password: "123456", password_confirmation: "123456", first_name: "Juan", last_name: "JuanJuan")
+
+6.times do
+  Car.create!(model: Faker::Vehicle.manufacture, price: 100, start_date: "20/12/2022", end_date: "02/02/2023", user_id: user.id)
+end
