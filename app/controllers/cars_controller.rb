6bb033a03c3d91062e91d3b2ccd3bd6473b43834
Gerard Mota <gmotacastillo@gmail.com>
2022-11-22 14:25:36 +0100

    added car model. pundit missing

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -22,6 +22,22 @@ class CarsController < ApplicationController
     end
   end
 
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path, status: :see_other
+  end
+
   private
 
   def car_params
