79f4ed26fcbd5678b282cafa5699ef78d5e8e8d6
Gerard Mota <gmotacastillo@gmail.com>
2022-11-24 17:46:31 +0100

    made search case insensitive, added booking seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,17 @@ require "faker"
 user = User.last
 
 2.times do
-  Car.create!(model: Faker::Vehicle.manufacture, price: 100, start_date: "28/11/2022", end_date: "02/02/2023", user_id: user.id, location: "Amsterdam", description: Faker::Vehicle.standard_specs)
+  Car.create!(model: Faker::Vehicle.manufacture, price: 100, start_date: "28/11/2022", end_date: "02/02/2023", user_id: user.id, location: "Amsterdam", description: Faker::Vehicle.version)
 end
 
 2.times do
-  Car.create!(model: Faker::Vehicle.manufacture, price: 150, start_date: "01/01/2023", end_date: "02/04/2023", user_id: user.id, location: "London", description: Faker::Vehicle.standard_specs)
+  Car.create!(model: Faker::Vehicle.manufacture, price: 150, start_date: "01/01/2023", end_date: "02/04/2023", user_id: user.id, location: "London", description: Faker::Vehicle.version)
 end
 
 2.times do
-  Car.create!(model: Faker::Vehicle.manufacture, price: 150, start_date: "01/01/2023", end_date: "02/04/2023", user_id: user.id, location: "Barcelona", description: Faker::Vehicle.standard_specs)
+  Car.create!(model: Faker::Vehicle.manufacture, price: 150, start_date: "01/01/2023", end_date: "02/04/2023", user_id: user.id, location: "Barcelona", description: Faker::Vehicle.version)
+end
+
+10.times do
+  Booking.create!(start_date: "25/11/2023", end_date: "25/11/2024", user_id: rand(1..5), car_id: 4)
 end
